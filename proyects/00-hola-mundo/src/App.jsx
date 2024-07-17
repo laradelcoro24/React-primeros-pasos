dfab38efd0cdb33dfc9ac13e46021a54cf9b6b57
Lara Del Coro <laradelcoro01@gmail.com>
2024-07-17 14:40:52 -0300

    cambio en app,jsx

--- proyects/00-hola-mundo/src/App.jsx
+++ proyects/00-hola-mundo/src/App.jsx
@@ -1,20 +1,46 @@
-
 import './App.css'
 import { TwitterFollowCard } from './TwittterFollowCard'
 
 export function App() {
-
+//users renderizados
+  const users=[
+    {
+      userName: 'laradelcoro24',
+      name: 'Lara Del Coro',
+      isFollowing: true
+    },
+    {
+      userName: 'gina12',
+      name: 'Gina Del Coro',
+      isFollowing: false
+    },
+    {
+      userName: 'marita16abat',
+      name: 'Maria Abatedaga',
+      isFollowing: true
+    }
+  ]
 
   return(
-
     <section className="App">
-      <TwitterFollowCard  userName="laradelcoro24" initialIsFollowing={true}>Lara Del Coro</TwitterFollowCard>
-      <TwitterFollowCard  userName="ginadelcoro">Gina Del Coro</TwitterFollowCard>
-      
+      {
+        users.map(user =>{
+          const {userName,name,isFollowing}= user
+          return(
+            
+            <TwitterFollowCard 
+            key={userName}
+            userName={userName} 
+            initialIsFollowing={isFollowing}>
+              {name}
+            </TwitterFollowCard>
+          )
+           
+          
+        })
+      }
 
-   
-  </section>
+    </section>
   )
-  
 
 }
